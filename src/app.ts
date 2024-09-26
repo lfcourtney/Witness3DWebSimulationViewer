1132bb625cc8e7e527c9e2b52d550404c36b9211
lfcourtney <lukechopper22@gmail.com>
2024-09-26 17:00:29 +0100

    Imported example mesh from remote location (raw.githubusercontent.com)
    - Basic app setup created

--- src/app.ts
+++ src/app.ts
@@ -7,70 +7,55 @@ import {
   ArcRotateCamera,
   Vector3,
   HemisphericLight,
-  Mesh,
   MeshBuilder,
   Tools,
+  SceneLoader,
+  AbstractMesh,
 } from '@babylonjs/core';
+import { blueMat } from './materials/colors';
 
-const loadEngine = (
-  canvas: HTMLCanvasElement
-): Promise<Engine | WebGPUEngine> => {
-  const returnEngine = new Promise<Engine | WebGPUEngine>(
-    async (resolve, reject) => {
-      if (navigator.gpu) {
-        var engine = new WebGPUEngine(canvas);
-        await engine.initAsync();
-        resolve(engine);
-      } else {
-        resolve(new Engine(canvas, true));
-      }
-    }
-  );
-  return returnEngine;
+const positionImportedMesh = (
+  importedMesh: AbstractMesh,
+  scaleFactor: number
+) => {
+  importedMesh.scaling = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+  const heightAdjustment =
+    0 - importedMesh.getBoundingInfo().boundingBox.minimumWorld.y;
+  importedMesh.position.y = heightAdjustment * scaleFactor;
 };
 
 class App {
+  readonly canvas: HTMLCanvasElement;
+  private engine: Engine | WebGPUEngine;
+
   constructor() {
-    // create the canvas html element and attach it to the webpage
-    var canvas = document.createElement('canvas');
-    canvas.style.width = '100%';
-    canvas.style.height = '100%';
-    canvas.id = 'gameCanvas';
-    document.body.appendChild(canvas);
+    this.canvas = this.createCanvas();
 
-    loadEngine(canvas).then((engine) => {
-      // This creates a basic Babylon Scene object (non-mesh)
-      var scene = new Scene(engine);
+    this.loadEngine().then((engine) => {
+      this.engine = engine;
 
-      var camera = new ArcRotateCamera(
-        'camera',
-        Tools.ToRadians(90),
-        Tools.ToRadians(65),
-        10,
-        Vector3.Zero(),
-        scene
+      const scene = this.createScene();
+
+      SceneLoader.ImportMesh(
+        '',
+        'https://raw.githubusercontent.com/lfcourtney/Witness3DWebSimulationViewerModels/main/VRNATIVE/example_obj/',
+        'teapot.obj',
+        scene,
+        function (newMeshes) {
+          const teapot = newMeshes[0];
+          positionImportedMesh(teapot, 0.5);
+          teapot.material = blueMat();
+        }
       );
 
-      // This attaches the camera to the canvas
-      camera.attachControl(canvas, true);
-
-      var light1: HemisphericLight = new HemisphericLight(
-        'light1',
-        new Vector3(0, 1, 0),
-        scene
-      );
-
-      // Default intensity is 1. Let's dim the light a small amount
-      light1.intensity = 0.7;
-
-      var sphere: Mesh = MeshBuilder.CreateSphere(
-        'sphere',
-        { diameter: 2 },
-        scene
-      );
+      // var sphere: Mesh = MeshBuilder.CreateSphere(
+      //   'sphere',
+      //   { diameter: 2 },
+      //   scene
+      // );
 
       // More sphere up one half of its height so it stays on the ground
-      sphere.position.y = 1;
+      // sphere.position.y = 1;
 
       var ground = MeshBuilder.CreateGround(
         'ground',
@@ -95,6 +80,75 @@ class App {
         scene.render();
       });
     });
+  }
+
+  private createCanvas(): HTMLCanvasElement {
+    // create the canvas html element and attach it to the webpage
+    var canvas = document.createElement('canvas');
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.id = 'gameCanvas';
+    document.body.appendChild(canvas);
+
+    return canvas;
+  }
+
+  private loadEngine(): Promise<Engine | WebGPUEngine> {
+    const returnEngine = new Promise<Engine | WebGPUEngine>(
+      async (resolve, reject) => {
+        if (navigator.gpu) {
+          var engine = new WebGPUEngine(this.canvas);
+          await engine.initAsync();
+          resolve(engine);
+        } else {
+          resolve(new Engine(this.canvas, true));
+        }
+      }
+    );
+    return returnEngine;
+  }
+
+  private addWindowResizeEventListener() {
+    // Watch for browser/canvas resize events
+    const engine = this.engine;
+    const canvas = this.canvas;
+    window.addEventListener('resize', function () {
+      engine.resize();
+      canvas.style.width = '100%';
+      canvas.style.height = '100%';
+    });
+  }
+
+  private createScene(): Scene {
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new Scene(this.engine);
+
+    var camera = new ArcRotateCamera(
+      'camera',
+      Tools.ToRadians(90),
+      Tools.ToRadians(65),
+      10,
+      Vector3.Zero(),
+      scene
+    );
+
+    camera.upperRadiusLimit = 40;
+    camera.lowerRadiusLimit = 5;
+    camera.upperBetaLimit = Tools.ToRadians(85);
+
+    // This attaches the camera to the canvas
+    camera.attachControl(this.canvas, true);
+
+    var light1: HemisphericLight = new HemisphericLight(
+      'light1',
+      new Vector3(0, 1, 0),
+      scene
+    );
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light1.intensity = 0.7;
+
+    return scene;
   }
 }
 new App();
