c9249669925f44cc8c3d527b2397a2f879db9c39
lfcourtney <lukechopper22@gmail.com>
2024-11-17 19:19:17 +0000

    Added comments to 'SimulationTagData' interface and began to add unit tests for simulationCreateTag

--- src/simulationTags/simulationTag.ts
+++ src/simulationTags/simulationTag.ts
@@ -1,6 +1,10 @@
 import { Scene } from "@babylonjs/core";
 import { MeshGeometry } from "../meshGeometry/meshGeometry";
 
+/**
+ * Interface specifically for formatting the data that every concrete class implementation of 'SimulationTagData' will have.
+ * To be precise, it contains the data from the wider Babylon.js scene that the class might need for performing its operations.
+ */
 export interface SimulationTagData {
   scene: Scene;
   geometriesMap: Map<string, MeshGeometry>;
