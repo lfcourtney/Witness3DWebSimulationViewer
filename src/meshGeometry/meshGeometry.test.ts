d8759926b1c5151e621e3b90fc2fe3f9629a2cee
lfcourtney <lukechopper22@gmail.com>
2024-11-27 23:12:40 +0000

    Added unit tests for MachineGeometry class

--- src/meshGeometry/meshGeometry.test.ts
+++ src/meshGeometry/meshGeometry.test.ts
@@ -25,6 +25,16 @@ function initialiseAbstractMesh() {
       y: 0,
       z: 0,
     },
+    rotation: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    scaling: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
   };
 }
 
@@ -88,5 +98,59 @@ describe("MeshGeometry class", () => {
 
     // Assert that 'getDescendants' has been called once
     expect(abstractMesh.getDescendants).toHaveBeenCalledOnce();
+  });
+
+  it("should apply position to 'transformMesh' when 'setPosition' method is invoked", () => {
+    // Arrange
+    const geometryObject = new MeshGeometry(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      abstractMesh as any,
+      exampleGeometryName,
+    );
+
+    const mockPosition = { x: 10, y: 10, z: 10 };
+
+    // Act
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (geometryObject as any).setPosition(mockPosition);
+
+    // Assert that position has been applied
+    expect(abstractMesh.position).toEqual(mockPosition);
+  });
+
+  it("should apply rotation to 'transformMesh' when 'setRotation' method is invoked", () => {
+    // Arrange
+    const geometryObject = new MeshGeometry(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      abstractMesh as any,
+      exampleGeometryName,
+    );
+
+    const mockRotation = { x: 10, y: 10, z: 10 };
+
+    // Act
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (geometryObject as any).setRotation(mockRotation);
+
+    // Assert that rotation has been applied
+    expect(abstractMesh.rotation).toEqual(mockRotation);
+  });
+
+  it("should apply scaling to 'transformMesh' when 'setScaling' method is invoked", () => {
+    // Arrange
+    const geometryObject = new MeshGeometry(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      abstractMesh as any,
+      exampleGeometryName,
+    );
+
+    const mockScaling = { x: 10, y: 10, z: 10 };
+
+    // Act
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (geometryObject as any).setScaling(mockScaling);
+
+    // Assert that scaling has been applied
+    expect(abstractMesh.scaling).toEqual(mockScaling);
   });
 });
